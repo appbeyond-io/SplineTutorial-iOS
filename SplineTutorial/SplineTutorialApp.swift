de8e712b6bf621ca4ba66135f40f9628bde506b2
Yossa Bourne <yossabourne@Terminox-MBP.local>
2025-05-05 23:28:39 +0700

    chore: first commit

---
+++ SplineTutorial/SplineTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplineTutorialApp.swift
+//  SplineTutorial
+//
+//  Created by Yossa Bourne on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SplineTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
