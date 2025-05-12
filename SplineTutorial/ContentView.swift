4dcd93f0ae976e0851ca24d7b21a234f87ca62e8
Yossa Bourne <yossabourne@192.168.1.8>
2025-05-12 15:58:01 +0700

    feat: add final code

--- SplineTutorial/ContentView.swift
+++ SplineTutorial/ContentView.swift
@@ -6,16 +6,37 @@
 //
 
 import SwiftUI
+import SplineRuntime
 
 struct ContentView: View {
+  
+  /// NOTE: Local URL
+  /// Make sure Spline file is added to app target and recheck filename and extension
+  let url = Bundle.main.url(forResource: "voice_interaction_animation", withExtension: "splineswift")
+  
+  /// NOTE: Remote URL
+  /// Make sure to change the Spline URL to yours. The URLs used in the tutorial were no longer available.
+//  let url = URL(string: "https://<your-spline-url-here>/scene.splineswift")!
+  
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+    ZStack {
+      SplineView(sceneFileURL: url)
+        .ignoresSafeArea()
+      
+      VStack {
+        Spacer()
+        
+        Text("Talk to Marble AI")
+          .font(.system(.title2, design: .default, weight: .semibold))
+          .foregroundStyle(.white)
+        
+        Image(systemName: "microphone.circle.fill")
+          .resizable()
+          .frame(width: 60, height: 60)
+          .foregroundStyle(.white, .blue)
+      }
+      .padding()
     }
-    .padding()
   }
 }
 
